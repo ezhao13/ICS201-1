d6bd5f525ab9ae6f77fae5aaf369a4b165b55306
ezhao13 <163616086+ezhao13@users.noreply.github.com>
2024-04-23 23:59:26 -0400

    Add files via upload

---
+++ main (11).py
@@ -0,0 +1,26 @@
+print("Carly’s makes the food that makes it a party.")
+print("************************************************")
+print("* Carly’s makes the food that makes it a party.*")
+print("************************************************")
+
+print("Sammy’s makes it fun in the sun.")
+print("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS")
+print("SSSSSSSSSSSSS Sammy’s makes it fun in the sun. SSSSSSSSSSSS")
+print("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS")
+
+for i in range(5):
+  print("hi")
+  
+print("*****   *****")
+print("*          *")
+print("***       *")
+print("*        *")
+print("*****   *****") 
+
+print("a     a^2   a^3")
+print("1     1     1")
+print("2     4     8")
+print("3     9     27")
+print("4     16    64")
+
+print((9.5*4.5-2.5*3)/(45.5-3.5))
